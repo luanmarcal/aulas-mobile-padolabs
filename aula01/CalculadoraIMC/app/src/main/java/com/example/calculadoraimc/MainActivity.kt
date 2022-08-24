ef4dd8b582442aa69d9d9f0fc2a1190279d88bcd
luanmarcal <luan.marcal@padolabs.com.br>
2022-08-24 17:37:42 -0300

    :books: adding program CalculatorIMC

--- aula01/CalculadoraIMC/app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ aula01/CalculadoraIMC/app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -1,11 +1,76 @@
 package com.example.calculadoraimc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val idade = findViewById<EditText>(R.id.IdadeID);
+        val altura = findViewById<EditText>(R.id.AlturaID);
+        val peso = findViewById<EditText>(R.id.PesoID);
+
+        val homem = findViewById<CheckBox>(R.id.HomemID);
+        val mulher = findViewById<CheckBox>(R.id.MulherID);
+
+        val resultado = findViewById<TextView>(R.id.ResultadoID);
+
+        val buttonEnviar = findViewById<Button>(R.id.buttonEnviar);
+        val buttonLimpar = findViewById<Button>(R.id.buttonLimpar);
+
+        homem.setOnClickListener(){
+            if(mulher.isChecked){
+                mulher.setChecked(false);
+            }
+        }
+
+        mulher.setOnClickListener(){
+            if(homem.isChecked){
+                homem.setChecked(false);
+            }
+        }
+
+        buttonEnviar.setOnClickListener(){
+            if(idade.text.isEmpty() && altura.text.isEmpty() && peso.text.isEmpty()){
+                resultado.setText("Por favor, informe os campos corretamente");
+            }else {
+
+                val idadeContent =  idade.text.toString().toInt();
+                var alturaContent =  altura.text.toString().toDouble();
+                val pesoContent =  peso.text.toString().toDouble();
+
+                if(alturaContent > 2.5){
+                    alturaContent = alturaContent/100;
+                }
+
+//                if(homem.isChecked){
+//                    if(idadeContent>=)
+//
+//                }
+
+                val resultadoContent = pesoContent / (alturaContent * alturaContent);
+
+                resultado.setText("IMC = " + String.format("%.2f", resultadoContent));
+
+            }
+
+        }
+
+        buttonLimpar.setOnClickListener(){
+            resultado.setText("");
+            idade.setText("");
+            altura.setText("");
+            peso.setText("");
+            peso.setText("");
+            homem.setChecked(false);
+            mulher.setChecked(false);
+        }
+
     }
 }
