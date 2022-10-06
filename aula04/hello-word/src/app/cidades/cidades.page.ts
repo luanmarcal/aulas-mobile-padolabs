0519a4d772ef2bae1f501f3a5a6ee13d9cb2b20a
luanmarcal <luan.marcal@padolabs.com.br>
2022-10-06 17:34:32 -0300

    adding new pages

---
+++ aula04/hello-word/src/app/cidades/cidades.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cidades',
+  templateUrl: './cidades.page.html',
+  styleUrls: ['./cidades.page.scss'],
+
+})
+export class CidadesPage implements OnInit {
+
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit() {
+  }
+
+  goToPage() {
+    this.router.navigate(['/home']);
+  }
+
+}
